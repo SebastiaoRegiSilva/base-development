db1deccd4cad0a527abb112f725934b3ef435e12
Sebastião Regi <swillian2009@gmail.com.br>
2023-04-19 22:27:17 +0100

    Arquivo para testes de funções.

---
+++ teste.sh
@@ -0,0 +1,10 @@
+<<comment
+Arquivo para testes gerais.
+comment
+echo "Cadastrar novo usuário - Nome"
+read name
+echo "$name"
+
+#Armazenar o conteúdo do chave em uma variável e copiar ela.
+teste = cp cat ~/.ssh/id_ed25519.pub
+echo "$teste"
